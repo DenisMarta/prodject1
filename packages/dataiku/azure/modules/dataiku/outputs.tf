eef15df7481766d415363c92629c107bb2f3cf6c
DenisMarta <152717722+DenisMarta@users.noreply.github.com>
2024-01-01 23:03:06 +0100

    Update outputs.tf

--- packages/dataiku/azure/modules/dataiku/outputs.tf
+++ packages/dataiku/azure/modules/dataiku/outputs.tf
@@ -2,6 +2,3 @@ output "dataiku_vm_public_ip" {
   value = azurerm_public_ip.vm_public_ip.ip_address
 }
 
-output "dataiku_vm_ssh_hostname" {
-  value = "${var.vm_root_user}@${azurerm_public_ip.vm_public_ip.ip_address}"
-}
